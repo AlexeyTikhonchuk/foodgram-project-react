561fda0a8fbc3e91c9f8dde25b0aec20ff150a3d
Alexey Tikhonchuk <alexeytikhonchuk@gmail.com>
2022-12-26 00:49:59 +0300

    1.0

---
+++ backend/api/urls.py
@@ -0,0 +1,33 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import (FavoriteViewSet, FollowViewSet, IngredientViewSet,
+                    RecipeViewSet, ShoppingCartViewSet, TagViewSet,
+                    UserViewSet)
+
+app_name = 'api'
+
+router_api = routers.DefaultRouter()
+
+router_api.register('users', UserViewSet)
+router_api.register('tags', TagViewSet)
+router_api.register('ingredients', IngredientViewSet)
+router_api.register('recipes', RecipeViewSet)
+
+urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router_api.urls)),
+    path('users/<int:user_id>/subscribe/', FollowViewSet.as_view({
+        'post': 'create',
+        'delete': 'destroy'
+    }), name='subscribe'),
+    path('recipes/<int:id>/shopping_cart/', ShoppingCartViewSet.as_view({
+        'post': 'create',
+        'delete': 'destroy'
+    }), name='shopping_cart'),
+    path('recipes/<int:id>/favorite/', FavoriteViewSet.as_view({
+        'post': 'create',
+        'delete': 'destroy'
+    }), name='favorite')
+]
